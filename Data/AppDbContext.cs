29c2a7881d3cadf09203af8af06ce12b2ae99a4d
FelipeCostaq <felipecostasiqu@gmail.com>
2026-02-17 19:22:22 -0300

    :card_file_box: connection at SQLite

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FoodDash.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodDash.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Menu> Menu { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Orders> Orders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options) => options.UseSqlite("DataSource=app.db;Cache=Shared");
+    }
+}
